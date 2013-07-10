b9199fbfbc4fe3cb8b033993fe060c63d477c843
middle8media <seth@middle8media.com>
2013-07-10 11:43:38 -0400

    collapse map tab

--- _site/js/modules/collapseMap.js
+++ _site/js/modules/collapseMap.js
@@ -1,6 +1,5 @@
 //collapse and expand map
 define(['jquery'], function($) {
-  $("#map-wrapper").addClass('map-expand');
   $("#collapse-map").click(function() {
     $('#map-wrapper').toggleClass('map-collapse')
       console.log('You clicked #collapse-map');
