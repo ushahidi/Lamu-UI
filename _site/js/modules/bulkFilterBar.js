33e11967d077c089a1accf4ee1ea56a592606eea
middle8media <seth@middle8media.com>
2013-07-10 09:09:05 -0400

    refactored list view bulk filter

--- _site/js/modules/bulkFilterBar.js
+++ _site/js/modules/bulkFilterBar.js
@@ -1,7 +1,17 @@
 //bulk filter toggle
 define(['jquery'], function($) {
-  $('.list-view-select-post .checkbox').click(function (){
-    $('.list-view-bulk-filter-bar').slideToggle(200);
-  console.log('You clicked .list-view-bulk-filter-bar');
+  $('.list-view-select-post').click(function (){
+      $checked = $('.list-view-select-post input[type="checkbox"]:checked')
+      if ($checked.length > 0)
+      {
+          $('.list-view-bulk-filter-bar').removeClass('hidden');
+          $('.list-view-bulk-filter-bar').addClass('visible');
+      }
+      else
+      {
+           $('.list-view-bulk-filter-bar').removeClass('visible');
+           $('.list-view-bulk-filter-bar').addClass('hidden');
+      }
+      console.log('You clicked .list-view-bulk-filter-bar');
   });
 });
