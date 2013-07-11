3a802deef26792fc671358c8a5f638ad2509eebf
middle8media <seth@middle8media.com>
2013-07-11 16:31:14 -0400

    hide/show map zoom controls on collapse/expand map

--- js/modules/collapseMap.js
+++ js/modules/collapseMap.js
@@ -4,6 +4,7 @@ define(['jquery'], function($) {
     $('#map').toggleClass('map-collapse');
       console.log('You clicked #collapse-map');
     $('#collapse-tab').text($('#collapse-tab').text() == 'collapse map' ? 'expand map' : 'collapse map');
+    $('.leaflet-container .leaflet-control-zoom').toggle();
     return false;
   });
 
