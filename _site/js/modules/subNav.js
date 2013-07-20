006cf49fc566b14c80d48a57b076fae5dfe81c58
middle8media <seth@middle8media.com>
2013-07-20 10:15:14 -0400

    removed fixed from top bar, bad UI IMO

--- _site/js/modules/subNav.js
+++ _site/js/modules/subNav.js
@@ -2,22 +2,54 @@
 define(['jquery'], function($) {
 
   $('#views-subnav').click(function (){
-    $('.views-subnav').slideToggle(200);
+    if ($('.sets-subnav').hasClass('visible'))
+    {
+      $('.sets-subnav').addClass('hidden');
+      $('.views-subnav').addClass('visible');
+    }
+    else
+    {
+      $('.views-subnav').toggleClass('hidden');
+    }
   console.log('You clicked #views-subnav');
   });
 
   $('#views-subnav-mobile').click(function (){
-    $('.views-subnav').slideToggle(200);
+    if ($('.sets-subnav').css('display', 'none'))
+    {
+      $('.sets-subnav').hide();
+      $('.views-subnav').show();
+    }
+    else
+    {
+      $('.views-subnav').slideToggle(200);
+    }
   console.log('You clicked #views-subnav-mobile');
   });
 
   $('#sets-subnav').click(function (){
-    $('.sets-subnav').slideToggle(200);
+    if ($('.views-subnav').css('display', 'none'))
+    {
+      $('.views-subnav').hide();
+      $('.sets-subnav').show();
+    }
+    else
+    {
+      $('.sets-subnav').slideToggle(200);
+    }
   console.log('You clicked #sets-subnav');
   });
 
   $('#sets-subnav-mobile').click(function (){
-    $('.sets-subnav').slideToggle(200);
+    if ($('.views-subnav').css('display', 'none'))
+    {
+      $('.views-subnav').hide();
+      $('.sets-subnav').show();
+    }
+    else
+    {
+      $('.sets-subnav').slideToggle(200);
+    }
   console.log('You clicked #sets-subnav-mobile');
   });
 
